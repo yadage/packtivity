899e59d52e00bc2141312d8cee9f96f45e60907d
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-03 17:19:53 -0400

    new loader

--- packtivity/loader.py
+++ packtivity/loader.py
@@ -1,5 +1,4 @@
 import capschemas
 
 def load(source, toplevel, schema_name = schema_name, schemadir = None, validate = True):
-    data = capschemas.load(source,toplevel,'packtivity/packtivity-schema',schemadir,validate)
-    return data
+    return capschemas.load(source,toplevel,schema_name,schemadir,validate)
