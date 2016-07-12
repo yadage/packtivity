bd1f15e594b59bc6032df9862d564164fcdcfc70
lukasheinrich <lukas.heinrich@gmail.com>
2016-07-12 15:00:26 +0200

    just commit

--- packtivity/__init__.py
+++ packtivity/__init__.py
@@ -1,19 +1,19 @@
 import pkg_resources
 import logging
 schemadir = pkg_resources.resource_filename('packtivity','schema')
-    
+
 def publish(publisher,attributes,context):
     pub_type   = publisher['publisher_type']
     from handlers.publisher_handlers import handlers as pub_handlers
     handler = pub_handlers[pub_type]
     return handler(publisher,attributes,context)
-    
+
 def build_job(process,attributes):
     proc_type =  process['process_type']
     from handlers.process_handlers import handlers as proc_handlers
     handler = proc_handlers[proc_type]
     return handler(process,attributes)
-    
+
 def run_in_env(nametag,environment,command,context):
     env_type = environment['environment_type']
     from handlers.environment_handlers import handlers as env_handlers
@@ -21,11 +21,14 @@ def run_in_env(nametag,environment,command,context):
     return handler(nametag,environment,context,command)
 
 def prepublish(step,attributes,context):
+    '''
+    attempts to prepublish output data, returns None if not possible
+    '''
     pub = step['publisher']
     if pub['publisher_type'] in ['frompar-pub','consant-pub']:
         return publish(pub,attributes,context)
     return None
-    
+
 class packtivity_callable(object):
     def __init__(self,uniquetag,step,attributes,context):
         self.uniquetag = uniquetag
@@ -33,7 +36,7 @@ class packtivity_callable(object):
         self.attributes = attributes
         self.context = context
         self.published_data = prepublish(self.step,self.attributes,self.context)
-    
+
     def __call__(self):
         log = logging.getLogger('step_logger_{}'.format(self.uniquetag))
         try:
@@ -47,7 +50,7 @@ class packtivity_callable(object):
         except:
             log.exception('%s raised exception',self.uniquetag)
             raise
-    
+
 def packtivity(uniquetag,step,attributes,context):
     p = packtivity_callable(uniquetag,step,attributes,context)
     return p()
