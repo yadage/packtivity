6da7cf2d39ae71bbe780aa70237f660aa54e6f2b
lukasheinrich <lukas.heinrich@gmail.com>
2016-12-15 17:39:53 +0100

    unit tests and travis

---
+++ tests/test_backends.py
@@ -0,0 +1,13 @@
+from packtivity.utils import backend_from_string
+
+def test_multiproc_fixed_ncpu_auto():
+    backend_from_string('multiproc:auto')
+
+def test_multiproc_fixed_ncpu_specific():
+    backend_from_string('multiproc:4')
+
+def test_celery():
+    backend_from_string('celery')
+
+# def test_ipcluster():
+#     backend_from_string('ipcluster')
---
+++ tests/test_loading.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+from jsonschema.exceptions import ValidationError
+from packtivity.cli import load_pack
+from capschemas import schemadir
+
+
+def test_cliload_valid():
+    load_pack('noop-test.yml', os.getcwd(), schemadir, True)
+
+def test_cliload_non_valid():
+    with pytest.raises(ValidationError):
+        load_pack('noop-test-invalid.yml', os.getcwd(), schemadir, True)
+
+def test_cliload_accept_non_valid():
+    load_pack('noop-test-invalid.yml', os.getcwd(), schemadir, False)
+
+def test_fromgithub_load():
+    load_pack('madgraph.yml', 'from-github/phenochain', schemadir, True)
+
+def test_jsonpointer_load():
+    load_pack('steps.yml#/prepare', 'from-github/higgsmcproduction' , schemadir, True)
