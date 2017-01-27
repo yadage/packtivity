1d19f3fe4faddd98b8bf41dcf2af996af8bac571
lukasheinrich <lukas.heinrich@gmail.com>
2017-01-27 13:19:01 -0800

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'packtivity',
-  version = '0.5.6',
+  version = '0.5.7',
   description = 'packtivity - general purpose schema + bindings for PROV activities',
   url = '',
   author = 'Lukas Heinrich',
