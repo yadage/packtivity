2fc91060b94ca1d9cc88cfb38261a31cf406e602
lukasheinrich <lukas.heinrich@gmail.com>
2017-03-16 19:37:13 +0100

    mock jq in docs

--- docs/conf.py
+++ docs/conf.py
@@ -346,3 +346,18 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+
+
+# Mocking Code
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['jq']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
