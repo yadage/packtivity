3ee24e6d31ae7a00a1e4eeff686b694a0dc8d060
lukasheinrich <lukas.heinrich@gmail.com>
2017-03-16 15:43:27 +0100

    add POSIX state moduel

---
+++ packtivity/statecontexts/posixfs_context.py
@@ -0,0 +1,56 @@
+import itertools
+import os
+import shutil
+import packtivity.utils as utils
+
+def contextualize_data(data,context):
+    '''
+    interpolate data string with context-specific information.
+    here, replace {workdir} with first read-write location
+    '''
+    try: 
+        workdir = context['readwrite'][0]
+        return data.format(workdir = workdir)
+    except AttributeError:
+        return data
+
+def merge_contexts(lhs,rhs):
+    return {
+        'readonly': lhs.get('readonly',[]) + rhs.get('readonly',[]),
+        'readwrite': lhs.get('readwrite',[]) + rhs.get('readwrite',[])
+    }
+
+def make_new_context(name,oldcontext = None, subdir = True, create = False):
+    '''
+    creates a new context from an existing context.
+
+    if subdir is True it declares a new read-write nested under the old
+    context's read-write and adds all read-write and read-only locations
+    of the old context as read-only. This is recommended as it makes rolling
+    back changes to the global state made in this context easy.
+
+    else the same readwrite/readonly configuration as the parent context is used
+
+    '''
+    if oldcontext is None:
+        new_readwrite = os.path.abspath(name)
+    else:
+        new_readwrite = '{}/{}'.format(oldcontext['readwrite'][0],name) if subdir else oldcontext['readwrite'][0]
+    if create:
+        utils.mkdir_p(new_readwrite)
+    newcontext = {
+        'nametag':name.replace('/','_'), #replace in case name is nested path
+    }
+    if subdir:
+        readonly =  [ro for ro in itertools.chain(oldcontext['readonly'],oldcontext['readwrite'])] if oldcontext else []
+        newcontext.update(readwrite = [new_readwrite], readonly = readonly)
+    else:
+        readonly = oldcontext['readwrite'] if oldcontext else []
+        newcontext.update(readwrite = oldcontext['readwrite'], readonly = oldcontext['readonly'])
+    return newcontext
+
+def reset_state(context):
+    '''delete readwriteable locations of this context'''
+    for rw in context['readwrite']:
+        shutil.rmtree(rw)
+        os.makedirs(rw)
