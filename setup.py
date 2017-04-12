36d1d296f510330fe4c73d4c4d2332b65320ca2e
lukasheinrich <lukas.heinrich@gmail.com>
2017-04-13 01:05:53 +0200

    Bump version: 0.5.12 → 0.5.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if not 'READTHEDOCS' in os.environ:
 
 setup(
   name = 'packtivity',
-  version = '0.5.12',
+  version = '0.5.13',
   description = 'packtivity - general purpose schema + bindings for PROV activities',
   url = '',
   author = 'Lukas Heinrich',
