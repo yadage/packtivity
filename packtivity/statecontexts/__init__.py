6b7ad30373e1f65dda3735617c1c2e38e24915c1
lukasheinrich <lukas.heinrich@gmail.com>
2017-08-01 18:10:39 +0200

    * py3 compat

--- packtivity/statecontexts/__init__.py
+++ packtivity/statecontexts/__init__.py
@@ -2,11 +2,11 @@ from .posixfs_context import LocalFSState,LocalFSProvider
 
 def load_state(jsondata):
 	if jsondata['state_type'] == 'localfs':
-		return posixfs_context.LocalFSState.fromJSON(jsondata)
+		return LocalFSState.fromJSON(jsondata)
 	raise TypeError('unknown state type {}'.format(jsondata['state_type']))
 
 def load_provider(jsondata):
 	if jsondata == None:
 		return None
 	if jsondata['state_provider_type'] == 'localfs_provider':
-		return posixfs_context.LocalFSProvider.fromJSON(jsondata)
+		return LocalFSProvider.fromJSON(jsondata)
