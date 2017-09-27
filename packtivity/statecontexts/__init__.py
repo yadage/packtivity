22fcc82ad0cae9b27c8bee0d496593c381a8cc81
lukasheinrich <lukas.heinrich@gmail.com>
2017-09-27 16:57:55 +0200

    state: load state from state plugin if defined

--- packtivity/statecontexts/__init__.py
+++ packtivity/statecontexts/__init__.py
@@ -1,12 +1,19 @@
+import os
+import importlib
 from .posixfs_context import LocalFSState,LocalFSProvider
 
 def load_state(jsondata):
-	if jsondata['state_type'] == 'localfs':
-		return LocalFSState.fromJSON(jsondata)
-	raise TypeError('unknown state type {}'.format(jsondata['state_type']))
+    if jsondata['state_type'] == 'localfs':
+        return LocalFSState.fromJSON(jsondata)
+    if 'PACKTIVITY_STATEPROVIDER' in os.environ:
+        module = importlib.import_module(os.environ['PACKTIVITY_STATEPROVIDER'])
+        return module.load_state(jsondata)
+
+
+    raise TypeError('unknown state type {}'.format(jsondata['state_type']))
 
 def load_provider(jsondata):
-	if jsondata == None:
-		return None
-	if jsondata['state_provider_type'] == 'localfs_provider':
-		return LocalFSProvider.fromJSON(jsondata)
+    if jsondata == None:
+        return None
+    if jsondata['state_provider_type'] == 'localfs_provider':
+        return LocalFSProvider.fromJSON(jsondata)
