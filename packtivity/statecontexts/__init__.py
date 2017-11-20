b3199289d9b9141f88c9b1eb9baedc60f5550054
lukasheinrich <lukas.heinrich@gmail.com>
2017-11-20 14:22:26 +0100

    * remove state provider .. this the responsibility of workflow engines, not packtivity itself   as it relates to orchestrating / genearting multiple states of multiple packtivities

--- packtivity/statecontexts/__init__.py
+++ packtivity/statecontexts/__init__.py
@@ -1,7 +1,7 @@
 import os
 import importlib
 import logging
-from .posixfs_context import LocalFSState,LocalFSProvider
+from .posixfs_context import LocalFSState
 
 log = logging.getLogger(__name__)
 
@@ -22,23 +22,3 @@ def load_state(jsondata,deserialization_opts = None):
     if jsondata['state_type'] == 'localfs':
         return LocalFSState.fromJSON(jsondata)
     raise TypeError('unknown state type {}'.format(jsondata['state_type']))
-
-def load_provider(jsondata,deserialization_opts = None):
-    log.debug('load_provider opts %s', deserialization_opts)
-    deserialization_opts = deserialization_opts or {}
-    if jsondata == None:
-        return None
-    if 'state_provider' in deserialization_opts:
-        providerstring = deserialization_opts.get('state_provider','')
-        if providerstring.startswith('py:'):
-            _, module, providerclass = providerstring.split(':')
-            module = importlib.import_module(module)
-            providerclass = getattr(module,providerclass)
-            provideropts = {}
-            return providerclass.fromJSON(jsondata,**provideropts)
-    if 'PACKTIVITY_STATEPROVIDER' in os.environ:
-        module = importlib.import_module(os.environ['PACKTIVITY_STATEPROVIDER'])
-        return module.load_provider(jsondata)
-    if jsondata['state_provider_type'] == 'localfs_provider':
-        return LocalFSProvider.fromJSON(jsondata)
-    raise TypeError('unknown provider type {}'.format(jsondata['state_provider_type']))
