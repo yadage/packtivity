990a8ad2a52316b173d1c248316383707fc8061a
lukasheinrich <lukas.heinrich@gmail.com>
2018-01-16 16:40:19 +0100

    morecoverage

---
+++ tests/test_environments.py
@@ -0,0 +1,16 @@
+from packtivity.handlers.publisher_handlers import handlers
+from packtivity.typedleafs import TypedLeafs
+
+from packtivity.handlers.environment_handlers import handlers
+from packtivity.syncbackends import finalize_inputs
+
+import logging
+import monkeypatch
+
+def test_docker_parmounts(tmpdir,basic_localfs_state, docker_env_parmounts):
+	state = basic_localfs_state
+	environment = docker_env_parmounts.spec['environment']
+
+	parameters, state = finalize_inputs(TypedLeafs({'outputfile': '{workdir}/hello.txt'}), state)
+ 	env = handlers[environment['environment_type']]['default'](environment,parameters,state)
+	assert env['par_mounts'][0]['mountcontent'] == '"{}"'.format(parameters['outputfile'])
---
+++ tests/test_typedleafs.py
@@ -0,0 +1,105 @@
+from packtivity.typedleafs import TypedLeafs
+class MyClass(object):
+    def __init__(self, first_attr, second_attr):
+        self.first_attr = first_attr
+        self.second_attr = second_attr
+
+    @classmethod
+    def fromJSON(cls, data):
+        return cls(**data)
+
+    def json(self):
+        return {
+            'first_attr': self.first_attr,
+            'second_attr': self.second_attr
+        }
+
+datamodel = {'keyword': '$type', 'types': {'MyClass': MyClass}}
+
+simple_data = {
+    'hello': {
+        '$type': 'MyClass',
+        'first_attr': 'hello',
+        'second_attr': 'world'
+    }
+}
+
+nested_data = {
+    'list_of_things': [{
+        '$type': 'MyClass',
+        'first_attr': 'hello',
+        'second_attr': 'world'
+    },
+    {
+        '$type': 'MyClass',
+        'first_attr': 'hello',
+        'second_attr': 'world'
+    }],
+    'single_thing': {
+        '$type': 'MyClass',
+        'first_attr': 'hello',
+        'second_attr': 'world'
+    }
+}
+
+def test_init():
+    tl = TypedLeafs(simple_data,datamodel)
+    assert type(tl['hello']) == MyClass
+    assert tl['hello'].first_attr == 'hello'
+    assert tl['hello'].second_attr == 'world'
+    assert tl.json() == simple_data
+
+    tl = TypedLeafs.fromJSON(simple_data, deserialization_opts = {'leafmodel': datamodel})
+    assert tl.json() == simple_data
+
+def test_deepnest():
+    tl = TypedLeafs(nested_data,datamodel)
+    paths = [p.path for p,v in tl.leafs()]
+    assert set(paths) == set(['/list_of_things/0','/list_of_things/1','/single_thing'])
+
+
+def test_jq():
+    tl = TypedLeafs(nested_data,datamodel)
+    assert tl.jq('.list_of_things[]',multiple_output = True)[0].json() == tl['list_of_things'][0].json()
+    assert tl.jq('.list_of_things[]',multiple_output = True)[1].json() == tl['list_of_things'][1].json()
+
+    assert tl.jq('[.list_of_things[]]').json() == nested_data['list_of_things']
+
+def test_jsonpath():
+    tl = TypedLeafs(nested_data,datamodel)
+
+    assert tl.jsonpath('single_thing').json() == tl['single_thing'].json()
+    assert tl.jsonpath('list_of_things[*]', multiple_output = True)[0].json() == tl['list_of_things'][0].json()
+
+def test_jsonpointer():
+    tl = TypedLeafs(nested_data,datamodel)
+    for p, v in tl.leafs():
+        try:
+            assert tl.jsonpointer(p.path).json() == v.json()
+        except AttributeError:
+            assert tl.jsonpointer(p.path) == v
+
+
+def test_refs():
+    import jq
+    refs = TypedLeafs(nested_data,datamodel).asrefs()
+    assert refs['list_of_things'][0].path == '/list_of_things/0'
+
+    import jsonpointer
+    jp = jsonpointer.JsonPointer('/list_of_things/0')
+
+    tl = TypedLeafs(nested_data,datamodel)
+    assert tl.resolve_ref(jp).json()  == tl['list_of_things'][0].json()
+
+
+def test_modify():
+    import jq
+    tl = TypedLeafs(nested_data,datamodel)
+    tlnew = TypedLeafs({'$type': 'MyClass', 'second_attr':'newsecond', 'first_attr': 'newfirst'}, datamodel)
+
+    tl['single_thing'] = tlnew.typed()
+    assert type(tlnew.typed()) == MyClass
+    assert tl['single_thing'].json() == tlnew.typed().json()
+
+    item = tl.pop('single_thing')
+    assert item.json() == tlnew.typed().json()
