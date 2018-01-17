42aa0351dc7091be7f4bc771137cd24e241535ad
Lukas <lukas.heinrich@gmail.com>
2018-01-17 11:35:37 +0100

    morecoverage (#29)
    * morecoverage

--- packtivity/handlers/environment_handlers.py
+++ packtivity/handlers/environment_handlers.py
@@ -4,9 +4,14 @@ handlers,environment = utils.handler_decorator()
 
 @environment('docker-encapsulated')
 def docker(environment,parameters,state):
+
+    jsonpars = parameters.json()
+    for p,v in parameters.leafs():
+        p.set(jsonpars, v)
+
     for i,x in enumerate(environment['par_mounts']):
         script = x.pop('jqscript')
-        x['mountcontent'] = jq.jq(script).transform(parameters, text_output = True)
+        x['mountcontent'] = jq.jq(script).transform(jsonpars, text_output = True)
 
     if environment['workdir'] is not None:
         environment['workdir'] = state.contextualize_value(environment['workdir'])
---
+++ tests/test_executions.py
@@ -0,0 +1,68 @@
+from packtivity.handlers.publisher_handlers import handlers
+from packtivity.typedleafs import TypedLeafs
+
+from packtivity.handlers.execution_handlers import run_docker_with_oneliner, docker_execution_cmdline
+import logging
+
+def test_docker_cvmfs(tmpdir,basic_localfs_state, docker_env_resources, monkeypatch):
+	state = basic_localfs_state
+	environment = docker_env_resources.spec['environment']
+	log = logging.getLogger('test')
+	job = {'command': 'echo hello world'}
+	container_argv, stdin = run_docker_with_oneliner(environment,job,log)
+
+	assert container_argv == ['sh','-c','echo hello world']
+	assert stdin == None
+
+	cmdline = docker_execution_cmdline(
+		state,environment,log,{'name':'myname'},
+		stdin = stdin,
+		cmd_argv = container_argv
+	)
+	assert '-v /cvmfs:/cvmfs' in cmdline
+
+	monkeypatch.setenv('PACKTIVITY_CVMFS_LOCATION','/here/cvmfs')
+	cmdline = docker_execution_cmdline(
+		state,environment,log,{'name':'myname'},
+		stdin = stdin,
+		cmd_argv = container_argv
+	)
+	assert '-v /here/cvmfs:/cvmfs' in cmdline
+
+	monkeypatch.setenv('PACKTIVITY_CVMFS_SOURCE','voldriver')
+	cmdline = docker_execution_cmdline(
+		state,environment,log,{'name':'myname'},
+		stdin = stdin,
+		cmd_argv = container_argv
+	)
+
+	assert '-v atlas-condb.cern.ch:/cvmfs/atlas-condb.cern.ch:rw' in cmdline
+	assert '-v sft.cern.ch:/cvmfs/sft.cern.ch:rw' in cmdline
+	assert '-v atlas.cern.ch:/cvmfs/atlas.cern.ch:rw' in cmdline
+	assert '--security-opt label:disable' in cmdline
+
+
+def test_docker_auth(tmpdir,basic_localfs_state, docker_env_resources, monkeypatch):
+	state = basic_localfs_state
+	environment = docker_env_resources.spec['environment']
+	log = logging.getLogger('test')
+	job = {'command': 'echo hello world'}
+	container_argv, stdin = run_docker_with_oneliner(environment,job,log)
+
+	assert container_argv == ['sh','-c','echo hello world']
+	assert stdin == None
+
+	cmdline = docker_execution_cmdline(
+		state,environment,log,{'name':'myname'},
+		stdin = stdin,
+		cmd_argv = container_argv
+	)
+	assert '-v /home/recast/recast_auth:/recast_auth:rw' in cmdline
+
+	monkeypatch.setenv('PACKTIVITY_AUTH_LOCATION','/here')
+	cmdline = docker_execution_cmdline(
+		state,environment,log,{'name':'myname'},
+		stdin = stdin,
+		cmd_argv = container_argv
+	)
+	assert '-v /here:/recast_auth:rw' in cmdline
