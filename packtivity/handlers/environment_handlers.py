3eb317ca64760a810963b3ce0f8a8f339e9392a5
lukasheinrich <lukas.heinrich@gmail.com>
2018-03-23 15:18:38 +0100

    * prevent environment handlers from mutating input

--- packtivity/handlers/environment_handlers.py
+++ packtivity/handlers/environment_handlers.py
@@ -1,9 +1,11 @@
 import packtivity.utils as utils
 import jq
+import copy
 handlers,environment = utils.handler_decorator()
 
 @environment('docker-encapsulated')
 def docker(environment,parameters,state):
+    environment = copy.deepcopy(environment)
 
     jsonpars = parameters.json()
     for p,v in parameters.leafs():
