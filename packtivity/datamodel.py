2405e17292f0c0a004a28a3a0ce393d93d976f68
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-02-12 01:20:49 +0100

    reinstate typedleafs

--- packtivity/datamodel.py
+++ packtivity/datamodel.py
@@ -1,8 +1,6 @@
+# from packtivity.datamodels.purejson import PureJsonModel
 from packtivity.typedleafs import TypedLeafs
 
 def create(data, model = None):
     return TypedLeafs(data,model)
-
-def leafs(data):
-    for p,v in data.leafs():
-        yield p,v
+#     return PureJsonModel(data,model)
