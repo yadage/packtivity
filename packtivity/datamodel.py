9dd56c9365716f86da195837b366b606494a37e5
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-02-12 03:37:30 +0100

    unadpurejson model

--- packtivity/datamodel.py
+++ packtivity/datamodel.py
@@ -1,6 +1,7 @@
 from packtivity.datamodels.purejson import PureJsonModel
 from packtivity.typedleafs import TypedLeafs
 
+assert PureJsonModel
 def create(data, model = None):
     return TypedLeafs(data,model)
     # return PureJsonModel(data,model)
