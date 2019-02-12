b96870cd6ea1d57ed0936d4ec0078af6f6b0582f
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-02-12 03:33:30 +0100

    unadd purejson model

--- packtivity/datamodel.py
+++ packtivity/datamodel.py
@@ -1,7 +1,7 @@
 from packtivity.datamodels.purejson import PureJsonModel
-# from packtivity.typedleafs import TypedLeafs
+from packtivity.typedleafs import TypedLeafs
 
 def create(data, model = None):
-#     return TypedLeafs(data,model)
-    return PureJsonModel(data,model)
+    return TypedLeafs(data,model)
+    # return PureJsonModel(data,model)
 
