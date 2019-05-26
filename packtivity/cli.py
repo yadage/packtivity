c4df88d8c7cb7ef84b5de6a74d048541786e7ec6
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-05-26 18:11:19 +0200

    py23

--- packtivity/cli.py
+++ packtivity/cli.py
@@ -135,7 +135,7 @@ def pubtest(spec,parfiles,state,parameter,read,write,toplevel,schemasource,valid
 @click.command()
 @click.argument('jsonfile')
 def checkproxy(jsonfile):
-    proxydata = json.safe_load(open(jsonfile))
+    proxydata = json.load(open(jsonfile))
     proxy, backend = bkutils.load_proxy(proxydata, best_effort_backend = True)
 
     ready = backend.ready(proxy)
