e5a3873cc4d8976dff743ed055ca4811a4cbbd4f
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-15 07:23:44 -0600

    ci: Update publishing workflow to use build and run on releases (#79)
    * Add MIT License
    * Add MANIFEST.in
    * Add python_requires to setup.py to require Python 2.7 or Python 3.6+
    * Add PyPI metadata classifiers to setup.py
    * Add workflow dispatch to CI
    * Update publishing workflow to use the build package to build an sdist
    and a wheel
    * Publish to TestPyPI on tags and publish to PyPI on GitHub releases

--- setup.py
+++ setup.py
@@ -28,9 +28,11 @@ setup(
     author="Lukas Heinrich",
     author_email="lukas.heinrich@cern.ch",
     packages=find_packages(),
+    # Support Python 3.6+ but keep legacy support for Python 2.7 until 2022
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     include_package_data=True,
     install_requires=deps,
-    extras_require={"celery": ["celery", "redis"],},
+    extras_require={"celery": ["celery", "redis"]},
     entry_points={
         "console_scripts": [
             "packtivity-run=packtivity.cli:runcli",
@@ -40,4 +42,19 @@ setup(
         ],
     },
     dependency_links=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Operating System :: OS Independent",
+    ],
 )
