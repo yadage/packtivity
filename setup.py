e891ffc7b4bafc453d74a16f384ccd203cbed709
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-17 17:56:19 -0600

    Bump version: 0.14.23 → 0.14.24

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if not "READTHEDOCS" in os.environ:
 
 setup(
     name="packtivity",
-    version="0.14.23",
+    version="0.14.24",
     description="packtivity - general purpose schema + bindings for PROV activities",
     url="https://github.com/yadage/packtivity",
     author="Lukas Heinrich",
